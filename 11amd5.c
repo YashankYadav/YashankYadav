466707bcf56d2d122d4cf298efe67f2847d8a5b2
Yashank Yadav <55084551+YashankYadav@users.noreply.github.com>
2020-04-15 16:01:29 +0530

    Add files via upload

---
+++ 11amd5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if((n%5==0) && (n%11==0))
+{
+printf("number is divisible by 5 and 11");
+}
+else
+{
+printf("number is not divisible by 5 or 11");
+}
+
+}
